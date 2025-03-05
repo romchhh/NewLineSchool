abbdaaf7cf9fb585a53b68a63027c93574283e28
nowayrm <nowayrm@MacBook-Pro-nowayrm.local>
2025-03-06 00:55:59 +0200

    Update vite.config.ts with additional build options

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
   },
   resolve: {
     alias: {
