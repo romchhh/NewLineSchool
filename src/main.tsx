120509b7bf7fae8c5503efd15c5d7f19b5e62077
nowayrm <nowayrm@MacBook-Pro-nowayrm.local>
2025-03-06 01:04:37 +0200

    Prepare for deployment to GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { HashRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Router>
     <App />
-  </StrictMode>,
-)
+  </Router>
+);
