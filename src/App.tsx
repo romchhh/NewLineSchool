594754fd3f825898945ba680a49b24ee0d28bb8d
nowayrm <nowayrm@MacBook-Pro-nowayrm.local>
2025-03-06 01:17:54 +0200

    Prepare for deployment to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { createBrowserRouter, RouterProvider } from "react-router"
-import Root from "./pages/Root"
-import Home from "./pages/Home"
-import ForSchoolchildren from "./pages/ForSchoolchildren"
-import ForAdults from "./pages/ForAdults"
-import PrepareForExams from "./pages/PrepareForExams"
-import OurClubs from "./pages/OurClubs"
-import TestsPage from "./pages/TestsPage"
-import TestStartingPage from "./pages/TestStartingPage"
-export default function App(){
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import ForSchoolchildren from "./pages/ForSchoolchildren";
+import ForAdults from "./pages/ForAdults";
+import PrepareForExams from "./pages/PrepareForExams";
+import OurClubs from "./pages/OurClubs";
+import TestsPage from "./pages/TestsPage";
+import TestStartingPage from "./pages/TestStartingPage";
+
+export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
@@ -47,8 +48,7 @@ export default function App(){
       path: "/tests/:testId",
       element: <TestStartingPage />
     }
-  ])
+  ]);
 
-
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
