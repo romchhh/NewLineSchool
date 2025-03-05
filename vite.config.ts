095ab4bc46454b13115a779071b2fec26e1680b3
nowayrm <nowayrm@MacBook-Pro-nowayrm.local>
2025-03-06 00:54:30 +0200

    Update vite.config.ts and prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/NewLineSchool/',
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
