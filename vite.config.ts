660393924237207867ff579e79206b5b3a8754db
nowayrm <nowayrm@MacBook-Pro-nowayrm.local>
2025-03-06 01:12:30 +0200

    Prepare for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NewLineSchool/',
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  base: '/NewLineSchool',
 })
